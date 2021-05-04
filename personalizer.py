c983c6f4bda7bdb25a7a2094705036da07d32865
rodanthi-alexiou <61449673+rodanthi-alexiou@users.noreply.github.com>
2021-05-04 23:23:17 +0300

    Update personalizer.py

--- personalizer.py
+++ personalizer.py
@@ -106,15 +106,4 @@ while keep_going:
         keep_going = False
 
 
-rank_request = RankRequest( actions=actions, context_features=context, excluded_actions=['juice'], event_id=eventid)
-response = client.rank(rank_request=rank_request)
 
-reward_val = "0.0"
-if(answer.lower()=='y'):
-    reward_val = "1.0"
-elif(answer.lower()=='n'):
-    reward_val = "0.0"
-else:
-    print("Entered choice is invalid. Service assumes that you didn't like the recommended food choice.")
-
-client.events.reward(event_id=eventid, value=reward_val)
